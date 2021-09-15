5079c77fe6321df4d4424cfe510f60c4192e1547
Moss Gallagher <moss@mossx.net>
2021-09-15 14:14:14 -0400

    Changed print Arguments

--- Example.c
+++ Example.c
@@ -1,19 +1,24 @@
 #include "FormatString.h"
-#include <string.h>
 #include <stdio.h>
 
 #ifdef _MSC_VER
 #define __COMPILER_STR__ "Visual Studio"
+#define __OS__ "Windows"
 #elif defined(__clang__)
 #define __COMPILER_STR__ "clang"
+#define __OS__ "Linux"
 #elif defined(__MINGW64__)
 #define __COMPILER_STR__ "MinGW 64-bit"
+#define __OS__ "Windows"
 #elif defined(__MINGW32__)
 #define __COMPILER_STR__ "MinGW 32-bit"
+#define __OS__ "Windows"
 #elif defined(__GNUC__)
 #define __COMPILER_STR__ "gcc"
+#define __OS__ "Windows"
 #else
 #define __COMPILER_STR__ "Unknown"
+#define __OS__ "Unknown"
 #endif
 int main() {
 	
